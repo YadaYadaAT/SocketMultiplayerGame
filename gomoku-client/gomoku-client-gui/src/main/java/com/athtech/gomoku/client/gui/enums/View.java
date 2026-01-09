c8f3b917a49f2e4cfc43d4bd5c72ed179edbe6b1
Balasis_Ioannis <giovani1994a@gmail.com>
2026-01-09 06:29:03 +0200

    [fix] an Uppercase(SignUp at view class instead of being Signup) was messing with the classpath

--- gomoku-client/gomoku-client-gui/src/main/java/com/athtech/gomoku/client/gui/enums/View.java
+++ gomoku-client/gomoku-client-gui/src/main/java/com/athtech/gomoku/client/gui/enums/View.java
@@ -3,7 +3,7 @@ package com.athtech.gomoku.client.gui.enums;
 public enum View {
     INTRO("/com/athtech/gomoku/client/gui/fxml/Intro.fxml"),
     LOGIN("/com/athtech/gomoku/client/gui/fxml/Login.fxml"),
-    SIGNUP("/com/athtech/gomoku/client/gui/fxml/SignUp.fxml"),
+    SIGNUP("/com/athtech/gomoku/client/gui/fxml/Signup.fxml"),
     LOBBY("/com/athtech/gomoku/client/gui/fxml/Lobby.fxml"),
     GAME("/com/athtech/gomoku/client/gui/fxml/Game.fxml"),
     SCENEWRAPPER("/com/athtech/gomoku/client/gui/fxml/SceneWrapper.fxml");
